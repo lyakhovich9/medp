b54977b3d72ca2ad52df4503813b585ce73aabcf
lyakhovich <lizenka37@gmail.com>
2024-02-27 17:14:58 +0300

    GII CRUD user

---
+++ views/site/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\User $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'fio')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'date_of_birth')->textInput() ?>
+
+    <?= $form->field($model, 'tel')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'role_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
