c5922ff2ba9c7c8c56ca994918315b792bdd4980
lyakhovich <lizenka37@gmail.com>
2024-02-28 14:46:19 +0300

    модель status

---
+++ models/Status.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "status".
+ *
+ * @property int $id
+ * @property string $name
+ *
+ * @property Reception[] $receptions
+ */
+class Status extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'status';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * Gets query for [[Receptions]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReceptions()
+    {
+        return $this->hasMany(Reception::class, ['status_id' => 'id']);
+    }
+}
